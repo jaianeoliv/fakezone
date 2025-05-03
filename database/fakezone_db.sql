dd83e8bd756a87dfc320fb0113aaeb9bb96b2055
jaianeoliv <blue.love02c@gmail.com>
2025-05-03 15:53:38 -0300

    Criação do CRUD

--- database/fakezone_db.sql
+++ database/fakezone_db.sql
@@ -13,12 +13,49 @@ INSERT INTO categorias (nome, descricao) VALUES
 ('Culinária', 'Receitas, dicas gastronômicas e cultura alimentar');
 
 
+SELECT * FROM usuarios;
+SELECT * FROM categorias;
 SELECT * FROM moods;
+SELECT * FROM posts;
+SELECT * FROM newsletter;
 
 ALTER TABLE usuarios ADD COLUMN role ENUM('admin', 'usuario') DEFAULT 'usuario';
 
 ALTER TABLE `posts`
 MODIFY COLUMN `data_atualizacao` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+
+INSERT INTO usuarios (nome_exibicao, username, email, senha)
+VALUES ('Jaiane', 'fakesnack', 'fakesnack333@gmail.com', 'fakezoneismine');
+
+UPDATE usuarios
+SET role = 'admin'
+WHERE username = 'fakesnack';
+
+INSERT INTO moods (descricao, emoji) VALUES
+('Feliz', '😃'),
+('Triste', '😢'),
+('Animado', '🤩'),
+('Pensativo', '🤔'),
+('Bravo', '😡');
+
+SHOW CREATE TABLE moods;
+ALTER TABLE moods CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE moods MODIFY emoji VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+
+SELECT p.id, p.titulo, p.conteudo, p.post_imagem, p.data_criacao, p.data_atualizacao, c.nome AS categoria, u.nome_exibicao AS usuario, m.nome AS mood
+FROM posts p
+JOIN categorias c ON p.categorias_id = c.id
+JOIN usuarios u ON p.usuarios_id = u.id
+JOIN moods m ON p.moods_id = m.id;
+
+
+
+SET GLOBAL time_zone = '-03:00';
+DELETE FROM usuarios WHERE id > 2;
+ALTER TABLE posts AUTO_INCREMENT = 1;
 
 
 
